7aab51a80276517b2a9f3196ee3b137b8292dbbf
BIT3444Demo <trewhite@deloitte.com>
2015-09-10 15:55:39 -0400

    Revert "Revert "Fix last commit""
    This reverts commit 8724905905a0b8b9382e4ca05f6dd826fc989fb5.

--- WestEndOrderForm/Form1.vb
+++ WestEndOrderForm/Form1.vb
@@ -35,7 +35,7 @@
         ClearReceipt()
         MsgBox("Trey's version")
     End Sub
-
+    'fix
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
