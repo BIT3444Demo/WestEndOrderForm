3c87bfe48625f0968a05b2d9697b1d78b5906a2c
Ethan Roberts <etroberts@deloitte.com>
2015-09-10 15:40:35 -0400

    added hello world to clear

--- WestEndOrderForm/Form1.vb
+++ WestEndOrderForm/Form1.vb
@@ -57,76 +57,76 @@
     Private Sub ComputeCurrentSelection()
         If radRibeyeSandwich.Checked Then
             subtotal += PRICE_RIBEYE_SANDWICH
-            Dim RibeyeSandwichItem As String = _
+            Dim RibeyeSandwichItem As String =
                 PRICE_RIBEYE_SANDWICH.ToString("C") & " - Ribeye Sandwich"
             lstOrderReceipt.Items.Add(RibeyeSandwichItem)
         End If
         If radLobster.Checked Then
             subtotal += PRICE_LOBSTER
-            Dim LobsterItem As String = _
+            Dim LobsterItem As String =
                 PRICE_LOBSTER.ToString("C") & " - Lobster"
             lstOrderReceipt.Items.Add(LobsterItem)
         End If
         If radHokieClub.Checked Then
             subtotal += PRICE_HOKIE_CLUB
-            Dim HokieClubItem As String = _
+            Dim HokieClubItem As String =
                 PRICE_HOKIE_CLUB.ToString("C") & " - Hokie Club"
             lstOrderReceipt.Items.Add(HokieClubItem)
         End If
         If radPubBurger.Checked Then
             subtotal += PRICE_PUB_BURGER
-            Dim PubBurgerItem As String = _
+            Dim PubBurgerItem As String =
                 PRICE_PUB_BURGER.ToString("C") & " - Pub Burger"
             lstOrderReceipt.Items.Add(PubBurgerItem)
         End If
 
         If chkCheese.Checked Then
             subtotal += PRICE_CHEESE
-            Dim CheeseItem As String = _
+            Dim CheeseItem As String =
                 PRICE_CHEESE.ToString("C") & " - Cheese"
             lstOrderReceipt.Items.Add(CheeseItem)
         End If
         If chkTomato.Checked Then
             subtotal += PRICE_TOMATO
-            Dim TomatoItem As String = _
+            Dim TomatoItem As String =
                 PRICE_TOMATO.ToString("C") & " - Tomato"
             lstOrderReceipt.Items.Add(TomatoItem)
         End If
 
         If radWater.Checked Then
             subtotal += PRICE_WATER
-            Dim WaterItem As String = _
+            Dim WaterItem As String =
                 PRICE_WATER.ToString("C") & " - Water"
             lstOrderReceipt.Items.Add(WaterItem)
         End If
         If radSoda.Checked Then
             subtotal += PRICE_SODA
-            Dim SodaItem As String = _
+            Dim SodaItem As String =
                 PRICE_SODA.ToString("C") & " - Soda"
             lstOrderReceipt.Items.Add(SodaItem)
         End If
         If radJuice.Checked Then
             subtotal += PRICE_JUICE
-            Dim JuiceItem As String = _
+            Dim JuiceItem As String =
                 PRICE_JUICE.ToString("C") & " - Juice"
             lstOrderReceipt.Items.Add(JuiceItem)
         End If
         If radSmoothie.Checked Then
             subtotal += PRICE_SMOOTHIE
-            Dim SmoothieItem As String = _
+            Dim SmoothieItem As String =
                 PRICE_SMOOTHIE.ToString("C") & " - Smoothie"
             lstOrderReceipt.Items.Add(SmoothieItem)
         End If
 
         If chkFries.Checked Then
             subtotal += PRICE_FRENCH_FRIES
-            Dim FrenchFriesItem As String = _
+            Dim FrenchFriesItem As String =
                 PRICE_FRENCH_FRIES.ToString("C") & " - French Fries"
             lstOrderReceipt.Items.Add(FrenchFriesItem)
         End If
         If chkMozzSticks.Checked Then
             subtotal += PRICE_MOZZARELLA_STICKS
-            Dim MozzSticksItem As String = _
+            Dim MozzSticksItem As String =
                 PRICE_MOZZARELLA_STICKS.ToString("C") & " - Mozzarella Sticks"
             lstOrderReceipt.Items.Add(MozzSticksItem)
         End If
@@ -154,6 +154,7 @@
         InitializeVariables()
         ClearCustomerSelections()
         ClearReceipt()
+        MsgBox("Hello world")
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
